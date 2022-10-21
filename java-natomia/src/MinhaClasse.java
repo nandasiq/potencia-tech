cd22b00e5d754eb5bb2191d2e5f19d85b5cd3092
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-21 12:54:56 -0300

    aulas de anatomia das classes 2 e 3

--- java-natomia/src/MinhaClasse.java
+++ java-natomia/src/MinhaClasse.java
@@ -2,6 +2,14 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
       System.out.println("olá, mundo"); 
+
+      
+      System.out.println(nomeCompleto("Luana", "Fernanda"));
     }
     
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+
+    }
 }
