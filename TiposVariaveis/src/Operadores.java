5702c2fd279e79fa59bb7064e69e8a02cfb0d3b8
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-24 12:14:27 -0300

    Operadores parte 2

--- TiposVariaveis/src/Operadores.java
+++ TiposVariaveis/src/Operadores.java
@@ -1,12 +1,31 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        int numero = 5;
-        numero = - numero;
-         System.out.println(numero);
+//        int numero = 5;
+//        numero = - numero;
+//         System.out.println(numero);
+//
+//         numero = (-1) * numero;
+//         System.out.println(numero);
 
-         numero = (-1) * numero;
+//        boolean variavel = true;        
+//        System.out.println(!variavel);
+//        variavel = !variavel;
+//        System.out.println(variavel);
 
-         System.out.println(numero);
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado;
+
+//        if (a == b) {
+//            resultado = "verdadeiro";
+//        } else {
+//            resultado = "falso";
+//        }
+
+        resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
     }
 }
