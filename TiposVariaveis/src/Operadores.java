e6dde90c2abb85674f6d445cf3c2f0bd15b65d5e
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-24 12:34:43 -0300

    Operadores fim

--- TiposVariaveis/src/Operadores.java
+++ TiposVariaveis/src/Operadores.java
@@ -1,6 +1,38 @@
 public class Operadores {
     
     public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2 ){
+            System.out.println("As fuas condicoes sao verdadeiras");
+        } else if (condicao1 || condicao2){
+            System.out.println("Uma das condicoes e verdadeira");
+        } else {
+            System.out.println("Nenhuma das condicoes sao verdadeira");
+        }
+        System.out.println("Fim");
+
+//        String nome1 = "Luana";
+//        String nome2 = new String("Luana");
+
+//        System.out.println(nome1 == nome2);
+//        System.out.println(nome1.equals(nome2));
+
+//        int numero1 = 1;
+//        int numero2 = 2;
+
+//        boolean simNao = numero1 == numero2;
+//        System.out.println("numero1 é igual numero2? " + simNao);
+
+//        simNao = numero1 != numero2;
+//        System.out.println("numero1 é diferente numero2? " + simNao);
+
+//        simNao = numero1 > numero2;
+//        System.out.println("numero1 é maior que numero2? " + simNao);
+
+
 //        int numero = 5;
 //        numero = - numero;
 //         System.out.println(numero);
@@ -13,10 +45,10 @@ public class Operadores {
 //        variavel = !variavel;
 //        System.out.println(variavel);
 
-        int a, b;
-        a = 5;
-        b = 6;
-        String resultado;
+//        int a, b;
+//        a = 5;
+//        b = 6;
+//        String resultado;
 
 //        if (a == b) {
 //            resultado = "verdadeiro";
@@ -24,8 +56,7 @@ public class Operadores {
 //            resultado = "falso";
 //        }
 
-        resultado = a == b ? "verdadeiro" : "falso";
-        System.out.println(resultado);
-
+//        resultado = a == b ? "verdadeiro" : "falso";
+//        System.out.println(resultado);
     }
 }
