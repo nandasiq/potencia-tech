67b5005b7f83ae91944d5c8abb7f7a391668b9c7
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-24 10:23:28 -0300

    tipos de variaveis

---
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+//tipo primitivos
+// String é classe
+
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        
+        System.out.println(numero);
+    
+        final double VALOR_PI = 3.14;
+    
+    }
+}
