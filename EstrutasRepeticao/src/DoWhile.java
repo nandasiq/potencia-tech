f961ba1a18404eb513fe448d41a3ad98ed327d6e
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-25 22:05:23 -0300

    loops e arrays. exercicios do-while

---
+++ EstrutasRepeticao/src/DoWhile.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+        /*
+         * Maior e Média: Faça um programa que leia 5 números e
+         *  informe o maior número e a média desses números.
+         */
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite cinco numeros: ");
+        int n1 = entrada.nextInt();
+        int n2 = entrada.nextInt();
+        int n3 = entrada.nextInt();
+        int n4 = entrada.nextInt();
+        int n5 = entrada.nextInt();
+
+        
+        
+        entrada.close();
+    }
+    
+}
