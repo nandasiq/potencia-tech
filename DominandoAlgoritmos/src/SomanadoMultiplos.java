16c36da39907f2fae49d7d294f6f950975b3b03c
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-28 17:36:28 -0300

    Desafio Somando Multiplos

---
+++ DominandoAlgoritmos/src/SomanadoMultiplos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SomanadoMultiplos {
+	public static void main(String[] args) {
+        int A, N;
+
+        Scanner input = new Scanner(System.in);
+        A = input.nextInt();
+        N = input.nextInt();
+
+        int soma = 0;
+        int i = 1;
+        int multiplo = 0;
+            
+          do {
+              multiplo = A * i;
+              soma = soma + multiplo;
+              i++;
+              
+          } while (multiplo < N);
+            
+          System.out.print(soma);
+          input.close();
+    }
+}
