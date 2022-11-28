ee4523de58350c3a8f1c43ce9b21af3fc6b6ee3d
Fernanda Siqueira <nandasiq@gmail.com>
2022-11-28 17:57:20 -0300

    aula sobre Configuration properties

--- primeiros-passps/src/main/java/dio/springboot/app/Remetente.java
+++ primeiros-passps/src/main/java/dio/springboot/app/Remetente.java
@@ -1,10 +1,27 @@
 package dio.springboot.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "remetente")
 public class Remetente {
 	private String nome;
 	private String email;
+	private List<Long> telefones;
+	
 	public String getNome() {
 		return nome;
+	}
+	public List<Long> getTelefones() {
+		return telefones;
+	}
+	public void setTelefones(List<Long> telefones) {
+		this.telefones = telefones;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
--- primeiros-passps/src/main/java/dio/springboot/app/SistemaMensagem.java
+++ primeiros-passps/src/main/java/dio/springboot/app/SistemaMensagem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class SistemaMensagem implements CommandLineRunner {
 
-	@Value("${name: NoReplyDIO}")
-	private String nome = "Fernanda Siqueira";
-	@Value("${email}")
-	private String email =  "fer@exemplo.com";
-	@Value("${telefones}")
-	private List<Long> telefones = new ArrayList<>(Arrays.asList(new Long[] {11998853214L}));
-	
+	@Autowired
+	private Remetente remetente;
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Mensagem enviado por: " + nome
-				+ "\nEmail: " + email
-				+ "\nCom tel: " + telefones);
+		System.out.println("Mensagem enviado por: " + remetente.getNome()
+				+ "\nEmail: " + remetente.getEmail()
+				+ "\nCom tel: " + remetente.getTelefones());
 		System.out.println("Seu cadastro foi aprovado");
 	}
 	
